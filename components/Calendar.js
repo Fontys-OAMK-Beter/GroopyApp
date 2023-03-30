c34e7338d9484211048c619eeae0938adb8920a0
AleksiTVT <t1koal01@students.oamk.fi>
2023-03-30 13:45:25 +0200

    display preset text when pressing highlighted day
    The text is displayed underneath "Date content"
    
    There's no functionality yet to add text for own
    highlighted days

--- components/Calendar.js
+++ components/Calendar.js
@@ -4,21 +4,35 @@ import CalendarPicker from 'react-native-calendar-picker';
 import moment from 'moment';
 
 const Calendar = () => {
-    const [selectedStartDate, setSelectedStartDate] = useState([])
     const [listOfHiglightedDays, setCustomDatesStyles] = useState([
         {
-            date: moment("09-03-2023", "DD-MM-YYYY"),
+            date: moment("09-03-2023", "DD-MM-YYYY", true),
             style: {backgroundColor: "#235885"},
             textStyle: {color: "#FFFFFF"},
-            text: ":D"
+            text: "Iron man at 18:00"
+        },
+        {
+            date: moment("16-03-2023", "DD-MM-YYYY", true),
+            style: {backgroundColor: "#235885"},
+            textStyle: {color: "#FFFFFF"},
+            text: "E.T. at 19:00"
         }
     ])
-    const [inputtedDate, setInputtedDate] = useState('') //proper name for the text variable
+    const [inputtedDate, setInputtedDate] = useState('')
+    const [displayedDateContent, setDisplayedDateContent] = useState('')
 
-    const onDateChange = (date) => {
-        setSelectedStartDate(date.format('LL'))
+    const pressingDay = (date) => {
+        displayDateContent(date, listOfHiglightedDays)
     }
-        
+
+    const displayDateContent = (date, listOfHiglightedDays) => {
+        for (i = 0; i <= (listOfHiglightedDays.length - 1); i++) { //checking if the pressed day matches with a date in the list of days assigned with content
+            if (date.format('L') == listOfHiglightedDays[i].date.format('L')) {
+                setDisplayedDateContent(listOfHiglightedDays[i].text) //setting the pressed date's content into a state variable that is used to display the content onto the screen
+            }
+        }
+    }
+
     useEffect(() => {
         addDayToList(inputtedDate, listOfHiglightedDays)
     }, [inputtedDate])
@@ -28,7 +42,7 @@ const Calendar = () => {
         tempList = listOfHiglightedDays
 
         newDateObject = {
-            date: moment(inputtedDate, "DD-MM-YYYY", true), //true means stricts mode
+            date: moment(inputtedDate, "DD-MM-YYYY", true), //true means strict mode
             style: { backgroundColor: "#235885" },
             textStyle: { color: "#FFFFFF" }
         }
@@ -39,18 +53,19 @@ const Calendar = () => {
     return (
         <View style={styles.container}>
             <CalendarPicker
-                onDateChange={onDateChange}
+                onDateChange={pressingDay}
                 customDatesStyles={listOfHiglightedDays}
             />
-
-            <View>
-                <Text>SELECTED DATE:{selectedStartDate}</Text>
-            </View>
 
             <View>
                 <Text>Give a date to highlight:</Text>
                 <Text>Format: DD-MM-YYYY</Text>
                 <TextInput onChangeText={(text) => setInputtedDate(text)} style={styles.textInputBox}/>
+            </View>
+
+            <View>
+                <Text>Date content:</Text>
+                <Text>{displayedDateContent}</Text>
             </View>
         </View>
     )
