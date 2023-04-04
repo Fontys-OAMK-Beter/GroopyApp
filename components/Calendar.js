26911b9bfad3463a93c84d8b8aea3dac620426ba
AleksiTVT <t1koal01@students.oamk.fi>
2023-04-04 10:05:55 +0300

    add toggle option to view dates assigned to group

--- components/Calendar.js
+++ components/Calendar.js
@@ -7,18 +7,28 @@ import { Button } from '@react-native-material/core';
 const Calendar = () => {
     const [listOfHiglightedDays, setListOfHighlightedDays] = useState([
         {
-            date: moment("09-03-2023", "DD-MM-YYYY", true),
+            date: moment("09-04-2023", "DD-MM-YYYY", true),
             style: {backgroundColor: "#235885"},
             textStyle: {color: "#FFFFFF"},
-            text: "Iron man at 18:00"
+            text: "Iron man at 18:00",
+            group: "1",
         },
         {
-            date: moment("16-03-2023", "DD-MM-YYYY", true),
+            date: moment("16-04-2023", "DD-MM-YYYY", true),
             style: {backgroundColor: "#235885"},
             textStyle: {color: "#FFFFFF"},
-            text: "E.T. at 19:00"
-        }
+            text: "E.T. at 19:00",
+            group: "2",
+        },
+        {
+            date: moment("22-04-2023", "DD-MM-YYYY", true),
+            style: {backgroundColor: "#235885"},
+            textStyle: {color: "#FFFFFF"},
+            text: "Spirited away at 20:00",
+            group: "3",
+        },
     ])
+    const [listOfToggledHighlightedDays, setListOfToggledHighlightedDays] = useState([])
     const [inputtedDate, setInputtedDate] = useState('')
     const [inputtedContent, setInputtedContent] = useState('')
     const [displayedDateContent, setDisplayedDateContent] = useState('')
@@ -53,11 +63,28 @@ const Calendar = () => {
         setListOfHighlightedDays( tempList => [...tempList, newDateObject]) // creating a new list of date objects by spreading the old list of date objects and adding a new date object to the end of the new list
     }
 
+    const displayToggledDays = (toggleOption) => {
+
+        tempList = []
+
+        if(toggleOption != "all"){
+            for(i = 0; i < listOfHiglightedDays.length; i++){
+                if(listOfHiglightedDays[i].group == toggleOption){
+                    tempList.push(listOfHiglightedDays[i])
+                }
+            }
+        }else{
+            tempList = listOfHiglightedDays
+        }
+
+        setListOfToggledHighlightedDays(tempList)
+    } 
+
     return (
         <View style={styles.container}>
             <CalendarPicker
                 onDateChange={pressingDay}
-                customDatesStyles={listOfHiglightedDays}
+                customDatesStyles={listOfToggledHighlightedDays}
             />
 
             <View>
@@ -72,13 +99,19 @@ const Calendar = () => {
                     <TextInput onChangeText={(aString) => setInputtedContent(aString)} style={styles.textInputBox}/>
                 </View>
                 <View>
-                    <Button onPress={() => { inputDateAndContent() }}/>
+                    <Button onPress={() => inputDateAndContent() }/>
                 </View>
             </View>
 
             <View>
-                <Text>Date content:</Text>
-                <Text>{displayedDateContent}</Text>
+                <Text>Date content: {displayedDateContent}</Text>
+            </View>
+
+            <View style={styles.buttonViewStyle}>
+                <Button title={'All events'} onPress={() => displayToggledDays('all')}/>
+                <Button title={'Group 1'} onPress={() => displayToggledDays('1')}/>
+                <Button title={'Group 2'} onPress={() => displayToggledDays('2')}/>
+                <Button title={'Group 3'} onPress={() => displayToggledDays('3')}/>
             </View>
         </View>
     )
@@ -88,12 +121,18 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#FFFFFF',
-        marginTop: 50,
+        marginTop: 10,
     },
     textInputBox: {
         borderWidth: 2,
-        borderColor: "#000000"
-    }
+        borderColor: "#000000",
+    },
+    buttonViewStyle: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 16,
+        marginTop: 16,
+    },
 });
 
 export default Calendar
