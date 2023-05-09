3d3bfee28fb16fc3d63c7a18a5e5d5ed7dfa78da
Miko <miko.prykari@gmail.com>
2023-05-09 20:17:40 +0300

    typo fix

--- components/UserPage.js
+++ components/UserPage.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import { Text, TouchableOpacity, View, StyleSheet } from 'react-native'
+import { Text, TouchableOpacity, View, StyleSheet, Modal } from 'react-native'
 import * as SS from 'expo-secure-store'
 import { ScrollView } from 'react-native-gesture-handler'
 import { Icon } from '@react-native-material/core'
@@ -9,6 +9,7 @@ import CalendarViewOnly from './CalendarViewOnly'
 const UserPage = ({ navigation }) => {
 
     const { setIsLoggedIn } = useContext(LoginContext)
+    const [modalIsOpen, setModalIsOpen] = useState(false)
 
 
     const logout = async () => {
@@ -16,8 +17,30 @@ const UserPage = ({ navigation }) => {
         setIsLoggedIn(false)
     }
 
+    const favouriteMovies = () => {
+        setModalIsOpen(!modalIsOpen)
+    }
+
     return (
         <ScrollView style={styles.container}>
+
+            <Modal animationType='slide'
+                transparent={true}
+                visible={modalIsOpen}
+                onRequestClose={() => setModalIsOpen(!modalIsOpen)}
+            >
+                <View style={styles.modalContainer}>
+                    <ScrollView style={{ marginBottom: 40, marginTop: 20 }}>
+                        <Text>xdddddddd</Text>
+                    </ScrollView>
+                    <TouchableOpacity style={styles.hideBtn}
+                        onPress={() => setGroupModalVisible(!groupModalVisible)}>
+                        <Text style={styles.modalText}>Cancel</Text>
+                    </TouchableOpacity>
+                </View>
+            </Modal>
+
+
             <View style={{ padding: "3%" }}>
                 <View style={{
                     width: 200,
@@ -33,19 +56,14 @@ const UserPage = ({ navigation }) => {
                     <Icon name="account-circle" color="red" size={200} />
                 </View>
                 <View style={{ marginTop: "7%" }}>
-                    {/* <TouchableOpacity onPress={() => { navigation.navigate("Calendar") }} style={styles.btnCont}>
-                        <Text style={styles.btnText}>Calendar</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => { navigation.navigate("PersonalInfo") }} style={styles.btnCont}>
-                        <Text style={styles.btnText}>Personal information</Text>
-                    </TouchableOpacity> */}
                     <View style={styles.calendarContainerOuter}>
                         <View style={styles.calendarContainer}>
                             <CalendarViewOnly />
                         </View>
                     </View>
-
-
+                    <TouchableOpacity onPress={() => favouriteMovies} style={styles.btnCont}>
+                        <Text style={styles.btnText}>View favourite movies</Text>
+                    </TouchableOpacity>
                     <TouchableOpacity onPress={() => logout()} style={styles.btnCont}>
                         <Text style={styles.btnText}>Logout</Text>
                     </TouchableOpacity>
@@ -64,10 +82,10 @@ const styles = StyleSheet.create({
     },
     btnCont: {
         backgroundColor: "rgba(156, 32, 23, 0.9)",
-        marginBottom: 8,
+        marginBottom: "1%",
         width: "98%",
         padding: 15,
-        marginTop: "5%",
+        marginTop: "2%",
         borderRadius: 5,
     },
     btnText: {
@@ -89,6 +107,37 @@ const styles = StyleSheet.create({
         backgroundColor: "rgba(156, 32, 23, 0.9)",
         borderRadius: 15,
         paddingHorizontal: 10,
-        marginBottom: "3%",
-    }
+        marginBottom: "5%",
+    },
+    modalContainer: {
+        flex: 1,
+        alignItems: 'center',
+        marginHorizontal: 7,
+        marginVertical: 30,
+        marginBottom: 100,
+        backgroundColor: 'rgba(0, 0, 0, 0.90)',
+        borderRadius: 15
+      },
+      modalText: {
+        fontSize: 19,
+        color: "white",
+        textShadowRadius: 4,
+        textShadowColor: "black"
+      },
+      modalTitle: {
+        fontSize: 34,
+        color: "rgb(176, 15, 4)",
+        textShadowRadius: 3,
+        textShadowColor: "black",
+        fontWeight: "bold"
+      },
+      hideBtn: {
+        bottom: 1,
+        marginBottom: 15,
+        backgroundColor: "rgb(176, 15, 4)",
+        borderRadius: 10,
+        paddingVertical: 15,
+        paddingHorizontal: 25,
+        fontSize: 20, 
+      }
 })
